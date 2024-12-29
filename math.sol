c13fc199530627aa1c2b65d87eb85e0aa16edbee
georgegathu <ggeorge.gathu@gmail.com>
2024-12-30 01:11:00 +0300

    Arithmetics

---
+++ math.sol
@@ -0,0 +1,23 @@
+/**
+Create a uint named dnaModulus, and set it equal to 10 to the power of dnaDigits.
+*/
+
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.5.0 <0.6.0;
+
+/**
+ * @title ArithmeticContract
+ * @author George Gathu
+ * @notice Adds 2 numbers
+ * @dev I'm just learning, forgive my errors
+ * @custom:
+ */
+contract ZombieFactory {
+
+	uint dnaDigits = 16;
+	uint dnaModulus = 10 ** dnaDigits;
+
+}
+
